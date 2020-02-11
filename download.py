6cfae4aa64b0f86e81d591b2358f0e00c4392b78
Richard Yan <Ruhua.Yan@mindgeek.com>
2020-02-11 17:55:27 -0500

    bs4 encode chinese chars

--- download.py
+++ download.py
@@ -14,42 +14,87 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 links = soup.find_all('a', class_='liebiao')
 title = '史记'
-ebook = Ebook(PATH_MOBI, title)
 
-for link in links:
-    print(link)
-    filename = link.get_text()
-    filePath = os.path.join(ebook.output_path, PATH_HTML, '{}.html'.format(filename))
-    
-    if os.path.isfile(filePath):
-        ebook.create_chapter(filename, filePath)
-        continue
+def download():
+    ebook = Ebook(PATH_MOBI, title)
+    for link in links:
+        print(link)
+        filename = link.get_text()
+        filePath = os.path.join(ebook.output_path, PATH_HTML, '{}.html'.format(filename))
+        
+        if os.path.isfile(filePath):
+            ebook.create_chapter(filename, filePath)
+            continue
 
-    # load page
-    page = requests.get(urllib.parse.urljoin(BASE_URL, link['href']))
-    soup = BeautifulSoup(page.content, 'html.parser')
-    # get page content
-    content = soup.select('#V')
-    if not content:
-        content = soup.select('#v')
-    if content:
-        content[0].div.extract()
+        # load page
+        page = requests.get(urllib.parse.urljoin(BASE_URL, link['href']))
+        soup = BeautifulSoup(page.content, 'html.parser')
+        # get page content
+        content = soup.select('#V')
+        if not content:
+            content = soup.select('#v')
+        if content:
+            content[0].div.extract()
+            
+            if not content[0].p:
+                c = BeautifulSoup('<p>{0} - No Content</p>'.format(link.get_text()))
+                print(c)
+            else: 
+                c = content[0]
+            
+            # remove body innerHTML
+            soup.body.clear()
+            if soup.head.link:
+                soup.head.link.extract()
+            soup.body.append(c)
+            
+            with open(filePath, mode='w', encoding='utf-8') as f:
+                f.write(soup.prettify())
+                ebook.create_chapter(filename, filePath) 
+    ebook.save()
+
+def download_onefile():
+    doc = '''
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+        <title>{}</title>
+    </head>
+    <body>
+    </body>
+    </html>
+    '''.format(title)
+    soup_doc = BeautifulSoup(doc, 'html.parser')
+
+    for link in links:
+        print(link)
+        chapter_name = link.get_text()
+        page = requests.get(urllib.parse.urljoin(BASE_URL, link['href']))
+        soup = BeautifulSoup(page.content, 'html.parser', from_encoding="gb18030")
+
+        # create chapter header
+        chapter_header = BeautifulSoup('<h2>{}</h2>'.format(chapter_name))
+        soup_doc.body.append(chapter_header)
+
+        # get page content
+        content = soup.select('#V')
+        if not content:
+            content = soup.select('#v')
+        if content:
+            content[0].div.extract()
         
         if not content[0].p:
-            c = BeautifulSoup('<p>{0} - No Content</p>'.format(link.get_text()))
+            c = BeautifulSoup('<p>{0} - No Content</p>'.format(chapter_name))
             print(c)
         else: 
             c = content[0]
-        
-        # remove body innerHTML
-        soup.body.clear()
-        if soup.head.link:
-            soup.head.link.extract()
-        soup.body.append(c)
-        
-        with open(filePath, mode='w', encoding='utf-8') as f:
-            f.write(soup.prettify())
-            ebook.create_chapter(filename, filePath) 
-ebook.save()
 
+        soup_doc.body.append(c)
 
+    file_path = os.path.join(PATH_HTML, '{}.html'.format(title))
+    with open(file_path, mode='w', encoding='utf-8') as f:
+            f.write(soup_doc.prettify())
+    
+
+if __name__ == '__main__': 
+    download_onefile()
