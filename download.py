9331dda2eac2ab9a700c296264274fc0b7c0d9ec
ruhuayan <ruhuayan@gmail.com>
2020-06-21 17:15:29 -0400

    scrape sub pages

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import requests
-import time
+# import time
 from bs4 import BeautifulSoup
 import urllib.parse
 import re
@@ -17,29 +17,50 @@ title = '史记'
 
 def download():
     ebook = Ebook(PATH_MOBI, title)
-    for link in links:
+    for link in links[104:]:
         print(link)
         filename = link.get_text()
         filePath = os.path.join(ebook.output_path, PATH_HTML, '{}.html'.format(filename))
         
         if os.path.isfile(filePath):
             ebook.create_chapter(filename, filePath)
-            continue
+            # continue
 
         # load page
         page = requests.get(urllib.parse.urljoin(BASE_URL, link['href']))
         soup = BeautifulSoup(page.content, 'html.parser', from_encoding="gb18030")
-    
+        pageLink = soup.find('p', class_='pageLink') #.find_all('a', href=True)
+
+        if pageLink:
+            sub_pages = pageLink.find_all('a', href=True)
+            pageLink.extract()
+
         c = get_content(soup)
         soup.body.clear()
         if soup.head.link:
             soup.head.link.extract()
+        
+        # if there are sub pages
+        if sub_pages:
+            hrefs = [l['href'] for l in sub_pages]
+            hrefs = list(set(hrefs))
+            hrefs.sort()
+            print(hrefs)
+            index = 0
+            for href in hrefs:
+                index += 1
+                print(index)
+                sub_page = requests.get(urllib.parse.urljoin(BASE_URL, href))
+                soup_link = BeautifulSoup(sub_page.content, 'html.parser', from_encoding="gb18030")
+                sub_cont = get_content(soup_link)
+                c.append(sub_cont)
         soup.body.append(c)
             
         with open(filePath, mode='w', encoding='utf-8') as f:
+            print(filePath)
             f.write(soup.prettify())
             ebook.create_chapter(filename, filePath) 
-    ebook.save()
+    # ebook.save()
 
 def download_onefile():
     import subprocess
@@ -66,7 +87,28 @@ def download_onefile():
         chapter_header = BeautifulSoup('<mbp:pagebreak/><h2 id="chapter-{}">{}</h2>'.format(index, chapter_name))
         soup_doc.body.append(chapter_header)
 
+        pageLink = soup.find('p', class_='pageLink') #.find_all('a', href=True)
+        sub_pages = None
+        if pageLink:
+            sub_pages = pageLink.find_all('a', href=True)
+            pageLink.extract()
+
         c = get_content(soup)
+
+        # if there are sub pages
+        if sub_pages:
+            hrefs = [l['href'] for l in sub_pages]
+            hrefs = list(set(hrefs))
+            hrefs.sort()
+            print(hrefs)
+            j = 0
+            for href in hrefs:
+                j += 1
+                print(j)
+                sub_page = requests.get(urllib.parse.urljoin(BASE_URL, href))
+                soup_link = BeautifulSoup(sub_page.content, 'html.parser', from_encoding="gb18030")
+                sub_cont = get_content(soup_link)
+                c.append(sub_cont)
         soup_doc.body.append(c)
 
     file_path = os.path.join(PATH_HTML, '{}.html'.format(title))
@@ -81,16 +123,21 @@ def download_onefile():
     
 def get_content(soup):
     # get page content
-    content = soup.select('#V')
+    content = soup.select('#V') or soup.select('#v')
 
-    if not content:
-        content = soup.select('#v')
+    # if not content:
+    #     content = soup.select('#v')
     if content:
         content[0].div.extract()
     
     if not content[0].p:
-        c = BeautifulSoup('<p>No Content</p>')
-        print(c)
+        paragraphes = soup.find_all('p')
+        c = BeautifulSoup()
+        for p in paragraphes:
+            if p.find('a'):
+                break
+            c.append(p)
+        # print(c)
     else: 
         c = content[0]
     return c
