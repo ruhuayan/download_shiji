eb560b314ce3f559b1892f85f294638d7c59f603
ruhuayan <ruhuayan@gmail.com>
2020-06-28 21:44:00 -0400

    create chapter class

--- download.py
+++ download.py
@@ -1,13 +1,11 @@
 import requests
-# import time
 from bs4 import BeautifulSoup
 import urllib.parse
-import re
 import os
-from ebook import Ebook
+from ebook import Chapter, Ebook
 
 BASE_URL = 'https://www.thn21.com'
-PATH_HTML = 'html'
+PATH_HTML = 'output'
 PATH_MOBI = 'mobi'
 page = requests.get('https://www.thn21.com/wen/famous/27038.html')
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -16,66 +14,9 @@ links = soup.find_all('a', class_='liebiao')
 title = '史记'
 
 def download():
-    ebook = Ebook(PATH_MOBI, title)
-    for link in links[104:]:
-        print(link)
-        filename = link.get_text()
-        filePath = os.path.join(ebook.output_path, PATH_HTML, '{}.html'.format(filename))
-        
-        if os.path.isfile(filePath):
-            ebook.create_chapter(filename, filePath)
-            # continue
+    
+    ebook = Ebook(PATH_HTML, title)
 
-        # load page
-        page = requests.get(urllib.parse.urljoin(BASE_URL, link['href']))
-        soup = BeautifulSoup(page.content, 'html.parser', from_encoding="gb18030")
-        pageLink = soup.find('p', class_='pageLink') #.find_all('a', href=True)
-
-        if pageLink:
-            sub_pages = pageLink.find_all('a', href=True)
-            pageLink.extract()
-
-        c = get_content(soup)
-        soup.body.clear()
-        if soup.head.link:
-            soup.head.link.extract()
-        
-        # if there are sub pages
-        if sub_pages:
-            hrefs = [l['href'] for l in sub_pages]
-            hrefs = list(set(hrefs))
-            hrefs.sort()
-            print(hrefs)
-            index = 0
-            for href in hrefs:
-                index += 1
-                print(index)
-                sub_page = requests.get(urllib.parse.urljoin(BASE_URL, href))
-                soup_link = BeautifulSoup(sub_page.content, 'html.parser', from_encoding="gb18030")
-                sub_cont = get_content(soup_link)
-                c.append(sub_cont)
-        soup.body.append(c)
-            
-        with open(filePath, mode='w', encoding='utf-8') as f:
-            print(filePath)
-            f.write(soup.prettify())
-            ebook.create_chapter(filename, filePath) 
-    # ebook.save()
-
-def download_onefile():
-    import subprocess
-    doc = '''
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
-        <title>{}</title>
-    </head>
-    <body>
-    </body>
-    </html>
-    '''.format(title)
-    soup_doc = BeautifulSoup(doc, 'html.parser')
-    index = 0
     for link in links:
         print(link)
         chapter_name = link.get_text()
@@ -83,11 +24,9 @@ def download_onefile():
         soup = BeautifulSoup(page.content, 'html.parser', from_encoding="gb18030")
 
         # create chapter header
-        index = index + 1
-        chapter_header = BeautifulSoup('<mbp:pagebreak/><h2 id="chapter-{}">{}</h2>'.format(index, chapter_name))
-        soup_doc.body.append(chapter_header)
+        chapter = Chapter(chapter_name)
 
-        pageLink = soup.find('p', class_='pageLink') #.find_all('a', href=True)
+        pageLink = soup.find('p', class_='pageLink')
         sub_pages = None
         if pageLink:
             sub_pages = pageLink.find_all('a', href=True)
@@ -101,32 +40,30 @@ def download_onefile():
             hrefs = list(set(hrefs))
             hrefs.sort()
             print(hrefs)
-            j = 0
+
             for href in hrefs:
-                j += 1
-                print(j)
+                
                 sub_page = requests.get(urllib.parse.urljoin(BASE_URL, href))
                 soup_link = BeautifulSoup(sub_page.content, 'html.parser', from_encoding="gb18030")
                 sub_cont = get_content(soup_link)
                 c.append(sub_cont)
-        soup_doc.body.append(c)
+        chapter.set_content(str(c))
+        ebook.add_chapter(chapter)
 
-    file_path = os.path.join(PATH_HTML, '{}.html'.format(title))
-    with open(file_path, mode='w', encoding='utf-8') as f:
-            f.write(soup_doc.prettify())
+    ebook.save()
+    # with open(file_path, mode='w', encoding='utf-8') as f:
+    #         f.write(soup_doc.prettify())
     # ebook-convert html to mobi
-    rc = subprocess.call([
-        'ebook-convert', file_path, os.path.join(PATH_HTML, '{}.mobi'.format(title)) 
-    ])
-    if rc != 0:
-        raise Exception('ebook-convert failed')
+    # rc = subprocess.call([
+    #     'ebook-convert', file_path, os.path.join(PATH_HTML, '{}.mobi'.format(title)) 
+    # ])
+    # if rc != 0:
+    #     raise Exception('ebook-convert failed')
     
 def get_content(soup):
     # get page content
     content = soup.select('#V') or soup.select('#v')
 
-    # if not content:
-    #     content = soup.select('#v')
     if content:
         content[0].div.extract()
     
@@ -137,10 +74,9 @@ def get_content(soup):
             if p.find('a'):
                 break
             c.append(p)
-        # print(c)
     else: 
         c = content[0]
     return c
 
 if __name__ == '__main__': 
-    download_onefile()
+    download()
--- ebook.py
+++ ebook.py
@@ -1,31 +1,32 @@
-# -*- coding: utf-8 -*-
-
-import tempfile
 import pathlib
-import time
 import shutil
-import platform
 import os
 import subprocess
-import io
-import sys
-import warnings
+from jinja2 import Environment, FileSystemLoader
 from typing import Optional, List
 
-from jinja2 import Environment, FileSystemLoader
+class Chapter:
+    index = 0
+    def __init__(self, title: str):
+        Chapter.index = Chapter.index + 1
+        self.id = 'chapter_{}'.format(Chapter.index)
+        self.title = title
+
+    def set_content(self, content):
+        self.content = content
 
 class Ebook:
 
     def __init__(self, path: str, title: str, author = 'ruhuayan.github.io'):
-        self.output_path = path # pathlib.Path(tempfile.gettempdir()) / str(time.time())
+        self.output_path = path
         self.title = title
         self.author = author
+        self.file_name = '{}.html'.format(title)
         self._templates_env = Environment(loader=FileSystemLoader(
             str(pathlib.Path(__file__).parent / 'templates/')))
         
         self.chapters = list()
-        self.chapter_order = 0
-        self._headings = None
+        self.chapter_index = 0
 
     def _render_file(self, template_name: str, context: dict, filename: str):
         template = self._templates_env.get_template(template_name)
@@ -38,6 +39,15 @@ class Ebook:
     def setAuthor(self, author: str):
         self.author = author
 
+    def getBookPath(self):
+        return os.path.join(self.output_path, self.file_name)
+
+    def getChapters(self) -> List:
+        return [dict(title = chapter.title, id = chapter.id) for chapter in self.chapters]
+
+    def add_chapter(self, chapter: Chapter): 
+        self.chapters.append(chapter)
+
     def save_cover(self) -> None:
         if self.cover_path:
             cover_path = self.cover_path
@@ -46,10 +56,22 @@ class Ebook:
             cover = pathlib.Path(__file__).parent / 'templates/cover.jpg'
             shutil.copy(str(cover), str(self.output_path))
 
-    def create_chapter(self, chapter_title: str, filepath: str):
-        self.chapter_order = self.chapter_order + 1
-        chapter = dict(title = chapter_title, path = filepath, play_order = self.chapter_order)
-        self.chapters.append(chapter)
+    def create_folder(self):
+        if not os.path.exists(self.output_path):
+            os.makedirs(self.output_path)
+
+    ''' Create main ebook html file '''
+    def _render_main(self) -> str:
+        html = '{}.html'.format(self.title)
+        self._render_file(
+            'template.html',
+            {
+                'title': self.title,
+                'chapters': self.chapters
+            },
+            html
+        )
+        return html
 
     def _render_toc_ncx(self):
         ncx = 'toc.ncx'
@@ -58,7 +80,8 @@ class Ebook:
             {
                 'chapters': self.chapters,
                 'title': self.title,
-                'author': self.author
+                'author': self.author,
+                'file_name': self.file_name
             },
             ncx
         )
@@ -66,7 +89,7 @@ class Ebook:
 
     def _render_toc_html(self):
         toc = 'toc.html'
-        self._render_file(toc, {'chapters': self.chapters}, toc)
+        self._render_file(toc, {'chapters': self.chapters, 'file_name': self.file_name}, toc)
         return toc
 
     def _render_opf(self) -> str:
@@ -74,7 +97,7 @@ class Ebook:
         self._render_file(
             'opf.xml',
             {
-                'chapters': self.chapters,
+                'file_name': self.file_name,
                 'title': self.title,
                 'author': self.author
             },
@@ -83,6 +106,8 @@ class Ebook:
         return opf_file
 
     def save(self) -> None:
+        self.create_folder()
+        self._render_main()
         self._render_toc_ncx()
         self._render_toc_html()
         opf_file = self._render_opf()
